dbe6fda57229fd1609acdeb901406ea0256cbc2f
Christopher Watkins <chrisbwatkins@gmail.com>
2021-02-14 13:41:00 -0700

    README generator

---
+++ index.js
@@ -0,0 +1,85 @@
+const { restoreDefaultPrompts } = require("inquirer");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type:"input",
+        message:"Project Title",
+        name:"title"
+    },
+    {
+        type:"input",
+        message:"a description",
+        name:"description"
+    },
+    {
+        type:"input",
+        message:"any needed dependencies?",
+        name:"installation"
+    },
+    {
+        type:"input",
+        message:"Why do we use these app?",
+        name:"usage"
+    },
+    {
+        type:"input",
+        message:"Any contribution guildlines",
+        name:"contribution"
+    },
+    {
+        type:"input",
+        message:"Test Instructions",
+        name:"tests"
+    },
+    {
+        type:"list",
+        message:"What license?",
+        choices:["MIT","IBM"],
+        name:"license"
+    },
+    {
+        type:"input",
+        message:"my Github",
+        name:"github"
+    },
+    {
+        type:"input",
+        message:"my email",
+        name:"email"
+    },
+
+
+];
+
+//table of contenst, installation, usage, license, contributing, testing instructions
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`,data,()=>{
+        
+    })
+
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+inquirer.prompt(questions).then((promise) => 
+// console.log(generateMarkdown(promise)),
+writeToFile(promise.title,generateMarkdown(promise))
+);
+}
+
+// Function call to initialize app
+init();
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,61 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+ function renderLicenseBadge(license) {
+  switch(license){
+      case("MIT"):
+        return  `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+      case("IBM"):
+        return  `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+        break;
+      
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+   return(` 
+  #♠${data.title}♦
+   ${data.description}
+
+  ## Table of Contents
+  [Installation](#installation)/
+  [Usage](#usage)/
+  [Contributions](#contribution)/
+  [Tests](#test)/
+  [License](#license)/
+  [Questions](#questions)
+  
+
+    
+  ## <h2 id="installation">Installation Instructions</h2>
+
+  ${data.installation}
+
+  ## <h2 id="usage">Usage</h2>
+   ${data.usage}
+
+  ## <h2 id="contribution">Contributions</h2>
+   ${data.contribution}
+  ## <h2 id="test">Tests</h2>
+  ${data.tests}
+  ## <h2 id="license"> License</h2>
+   ${renderLicenseBadge(data.license)};
+  ## <h2 id="questions"> Questions </h2>
+  for any questions please contact me at
+   [Github](https://github.com/Tevvels)
+   or at email:chrisbwatkins@gmail.com
+
+`);
+   
+}
+
+module.exports = generateMarkdown;
